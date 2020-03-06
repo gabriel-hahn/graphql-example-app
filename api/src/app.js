fd28a18f4667b49ec4ae0f649ee315f696eb81c6
Gabriel Hahn Schaeffer <gabriel_hahn@hotmail.com>
2020-03-06 11:43:04 -0300

    New mutations for author and books

--- api/src/app.js
+++ api/src/app.js
@@ -8,7 +8,7 @@ const schema = require('./schema/schema');
 const app = express();
 
 // connect to mlab database
-mongoose.connect(`mongodb+srv://${process.env.MONGODB_USER}:${process.env.MONGODB_PASSWORD}@gql-ninja-v7wek.mongodb.net/test?retryWrites=true&w=majority`);
+mongoose.connect(`mongodb://${process.env.MONGODB_USER}:${process.env.MONGODB_PASSWORD}@gql-ninja-shard-00-00-v7wek.mongodb.net:27017,gql-ninja-shard-00-01-v7wek.mongodb.net:27017,gql-ninja-shard-00-02-v7wek.mongodb.net:27017/test?ssl=true&replicaSet=gql-ninja-shard-0&authSource=admin&retryWrites=true&w=majority`);
 mongoose.connection.once('open', () => {
   console.log('Connected to database');
 });
