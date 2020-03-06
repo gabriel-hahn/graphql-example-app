11dfb4f50a511275478722ef3f77a125c9c7d028
Gabriel Hahn Schaeffer <gabriel_hahn@hotmail.com>
2020-03-06 11:06:06 -0300

    Mongoose connection

--- api/src/app.js
+++ api/src/app.js
@@ -1,8 +1,17 @@
+require('dotenv').config({path: __dirname + '/.env'})
+
 const express = require('express');
 const graphqlHTTP = require('express-graphql');
+const mongoose = require('mongoose');
 const schema = require('./schema/schema');
 
 const app = express();
+
+// connect to mlab database
+mongoose.connect(`mongodb+srv://${process.env.MONGODB_USER}:${process.env.MONGODB_PASSWORD}@gql-ninja-v7wek.mongodb.net/test?retryWrites=true&w=majority`);
+mongoose.connection.once('open', () => {
+  console.log('Connected to database');
+});
 
 app.use('/graphql', graphqlHTTP({
   schema,
