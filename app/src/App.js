e36d50b95c992de87efcf061323d0fef9635c3a4
Gabriel Hahn Schaeffer <gabriel_hahn@hotmail.com>
2020-03-09 10:13:47 -0300

    Setup graphql on react app

--- app/src/App.js
+++ app/src/App.js
@@ -1,10 +1,23 @@
 import React from 'react';
+import ApolloClient from 'apollo-boost';
+import { ApolloProvider } from 'react-apollo';
+
+import BookList from './components/BookList';
+
+const client = new ApolloClient({
+  uri: 'http://localhost:4000/graphql'
+});
+
+import './styles/global.css';
 
 function App() {
   return (
-    <div>
-      GraphQL React App
-    </div>
+    <ApolloProvider client={client}>
+      <div className="main">
+        <h1>Reading List</h1>
+        <BookList />
+      </div>
+    </ApolloProvider>
   );
 }
 
