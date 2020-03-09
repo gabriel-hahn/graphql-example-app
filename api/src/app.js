d37d05901ac849ed8896c4f75a4e90a763007fc9
Gabriel Hahn Schaeffer <gabriel_hahn@hotmail.com>
2020-03-09 11:00:47 -0300

    CORS added

--- api/src/app.js
+++ api/src/app.js
@@ -3,9 +3,12 @@ require('dotenv').config({path: __dirname + '/.env'})
 const express = require('express');
 const graphqlHTTP = require('express-graphql');
 const mongoose = require('mongoose');
+const cors = require('cors');
 const schema = require('./schema/schema');
 
 const app = express();
+
+app.use(cors());
 
 // connect to mlab database
 mongoose.connect(`mongodb://${process.env.MONGODB_USER}:${process.env.MONGODB_PASSWORD}@gql-ninja-shard-00-00-v7wek.mongodb.net:27017,gql-ninja-shard-00-01-v7wek.mongodb.net:27017,gql-ninja-shard-00-02-v7wek.mongodb.net:27017/test?ssl=true&replicaSet=gql-ninja-shard-0&authSource=admin&retryWrites=true&w=majority`);
